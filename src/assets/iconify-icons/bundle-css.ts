a9e8a5502200409de3e8e100af880dc20440fcaa
vphucnn <140361269+vphucnn@users.noreply.github.com>
2024-08-02 09:59:03 +0700

    init project

---
+++ src/assets/iconify-icons/bundle-css.ts
@@ -0,0 +1,42 @@
+import { readFile, writeFile } from 'node:fs/promises';
+
+import { getIconsCSS } from '@iconify/utils';
+import { locate } from '@iconify/json';
+
+/**
+ * List of icons. Key is icon set prefix, value is array of icons
+ *
+ * @type {Record<string, string[]>}
+ */
+const icons:  
+ Record<string, string[]> = {
+  'mdi': ['home', 'menu'],
+  'mdi-light': ['alert-circle', 'circle', 'help-circle'],
+};
+
+async function main() {
+  let code = '';
+
+  for (const prefix in icons) {
+    // Find location of .json file
+    const filename = locate(prefix);
+
+    // Load file and parse it
+    /** @type {import("@iconify/types").IconifyJSON} **/
+    const iconSet = JSON.parse(await readFile(filename, 'utf8'));
+
+    // Get CSS
+    const css = getIconsCSS(iconSet, icons[prefix]);
+
+    // Add it to code
+    code += css;
+  }
+
+  // Save CSS file
+  await writeFile('generated-icons.css',  
+ code, 'utf8');
+  console.log(`Saved CSS (${code.length} bytes)`);  
+
+}
+
+main();
